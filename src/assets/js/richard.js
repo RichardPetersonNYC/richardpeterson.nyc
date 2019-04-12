0b3b1dc012466ba0124582634aaea35f4e0c3355
Draxs <draxs75@gmail.com>
2019-04-12 20:07:17 +0300

    update

---
+++ src/assets/js/richard.js
@@ -0,0 +1,38 @@
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".header-logo").removeClass("light-logo");
+        $(".header-logo").addClass("dark-logo");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".header-logo").removeClass("dark-logo");
+        $(".header-logo").addClass("light-logo");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+$(function() {
+    var currLang = document.documentElement.lang;
+    $('#language_select select').on('change', function() {
+        var lang = $(this).val();
+        window.location.href = '/' + (lang === 'en' ? '' : lang);
+    });
+    $(".nav-button").click(function() {
+        $(".nav-button,.primary-nav").toggleClass("open");
+    });
+   
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 800, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
